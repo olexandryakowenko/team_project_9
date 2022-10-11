b1f0e67925190c77574651d80ec520b3f9fd7aa3
olexandryakowenko <109909171+olexandryakowenko@users.noreply.github.com>
2022-10-11 12:24:13 +0000

    Deploying to gh-pages from @ olexandryakowenko/team_project_9@16a2567e30f91f3386854553ba115feb96618c54 🚀

---
+++ index.1da5d37f.js
@@ -0,0 +1,2 @@
+$(".slider-feedback").slick({dots:!0,arrows:!1,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,autoplay:!0});
+//# sourceMappingURL=index.1da5d37f.js.map
---
+++ index.b25a82b1.js
@@ -0,0 +1,2 @@
+$(".slider-feedback").slick({dots:!0,arrows:!1,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,autoplay:!0});
+//# sourceMappingURL=index.b25a82b1.js.map
