6d61d4aa53b1b59f9b8e078fb91cac4086ef420b
olexandryakowenko <109909171+olexandryakowenko@users.noreply.github.com>
2022-10-13 20:46:50 +0300

    add connect

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-// import './js/anchor-scroll';
-// import './js/header-scroll';
-// import './js/simpleLightbox';
+import './js/anchor-scroll';
+import './js/header-scroll';
+import './js/simpleLightbox';
+import './js/mobile-menu';
+import './js/adresscard';
