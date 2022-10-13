000ab3b2b733d292d592f1369fecdd444113d31c
Daryna Morhunova <darusya0501@gmail.com>
2022-10-13 22:22:12 +0200

    fix modal

--- src/js/ingredients-1.js
+++ src/js/ingredients-1.js
@@ -1,14 +1,14 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-1-open]"),
-      closeModalBtn: document.querySelector("[data-modal-1-close]"),
-      modal: document.querySelector("[data-modal-1]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-icecream-open]"),
+    closeModalBtn: document.querySelector("[data-modal-icecream-close]"),
+    modal: document.querySelector("[data-modal-icecream]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
