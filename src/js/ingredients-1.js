3216b281ebee92512c91260caa8ccfbc2ec8d171
Daryna Morhunova <darusya0501@gmail.com>
2022-10-13 20:45:56 +0200

    change modal

--- src/js/ingredients-1.js
+++ src/js/ingredients-1.js
@@ -1,8 +1,8 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
+      openModalBtn: document.querySelector("[data-modal-1-open]"),
+      closeModalBtn: document.querySelector("[data-modal-1-close]"),
+      modal: document.querySelector("[data-modal-1]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
