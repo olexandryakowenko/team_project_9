4f6aa33fc8e398b1917747a18966525d18181ed0
olexandryakowenko <109909171+olexandryakowenko@users.noreply.github.com>
2022-10-14 07:13:13 +0000

    Deploying to gh-pages from @ olexandryakowenko/team_project_9@b340a114b200b1abad9443127a485895bec43251 🚀

---
+++ index.0bb3f53d.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalButton:document.querySelector("[data-modal-open]"),closeModalButton:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalButton.addEventListener("click",e),o.closeModalButton.addEventListener("click",e)}();
+//# sourceMappingURL=index.0bb3f53d.js.map
---
+++ index.413537d6.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalButton:document.querySelector("[data-modal-open]"),closeModalButton:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalButton.addEventListener("click",e),o.closeModalButton.addEventListener("click",e)})();
+//# sourceMappingURL=index.413537d6.js.map
