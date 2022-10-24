eb93dffead9f773d1ffd942cd7f427e7c0f4317d
olexandryakowenko <109909171+olexandryakowenko@users.noreply.github.com>
2022-10-24 18:29:34 +0000

    Deploying to gh-pages from @ olexandryakowenko/team_project_9@81c27acb3d465b94080dcea11892677083e726f9 🚀

---
+++ index.b8f1b318.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){window.screen.width>=768?scrollY<=70?document.getElementById("header").classList.remove("header__scroll"):document.getElementById("header").classList.add("header__scroll"):scrollY<=120?document.getElementById("header").classList.remove("header__scroll"):document.getElementById("header").classList.add("header__scroll")};
+//# sourceMappingURL=index.b8f1b318.js.map
