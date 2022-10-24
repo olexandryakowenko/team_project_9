c0e91b74a0fa3a71f12e10b0741a0410898295b6
Daryna Morhunova <darusya0501@gmail.com>
2022-10-24 11:40:17 +0200

    fix buy-now

--- src/js/buy-now-modal.js
+++ src/js/buy-now-modal.js
@@ -1,14 +1,17 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-buy-open]"),
-      closeModalBtn: document.querySelector("[data-modal-buy-close]"),
-      modal: document.querySelector("[data-modal-buy]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+  const refs = {
+    closeModalBtn: document.querySelector('[data-modal-buy-close]'),
+    modal: document.querySelector('[data-modal-buy]'),
+  };
+
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+  document.addEventListener('click', e => {
+    if (e.target.matches('[data-modal-buy-open]')) {
+      toggleModal();
     }
-  })();
+  });
+})();
