81c27acb3d465b94080dcea11892677083e726f9
olexandryakowenko <109909171+olexandryakowenko@users.noreply.github.com>
2022-10-24 21:28:48 +0300

    підключення script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import "./js/ingredients-1";
-import './js/anchor-scroll';
-import './js/header-scroll';
-import './js/simpleLightbox';
-import './js/mobile-menu';
-import './js/adresscard';
+// import "./js/ingredients-1";
+// import './js/anchor-scroll';
+// import './js/header-scroll';
+// import './js/simpleLightbox';
+// import './js/mobile-menu';
+// import './js/adresscard';
 
