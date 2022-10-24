cfa8927db88138a0cd1d3dfccf5fb40bd8edd814
olexandryakowenko <109909171+olexandryakowenko@users.noreply.github.com>
2022-10-24 17:48:07 +0000

    Deploying to gh-pages from @ olexandryakowenko/team_project_9@61aca85161f8eafa3d23798d8f92e5667495c7e0 🚀

---
+++ index.87e6ba48.js
@@ -0,0 +1,2 @@
+!function(){var e={closeModalBtn:document.querySelector("[data-modal-buy-close]"),modal:document.querySelector("[data-modal-buy]")};function t(){e.modal.classList.toggle("is-hidden")}e.closeModalBtn.addEventListener("click",t),document.addEventListener("click",(function(e){e.target.matches("[data-modal-buy-open]")&&t()}))}();
+//# sourceMappingURL=index.87e6ba48.js.map
---
+++ index.aee99c79.js
@@ -0,0 +1,2 @@
+(()=>{const e={closeModalBtn:document.querySelector("[data-modal-buy-close]"),modal:document.querySelector("[data-modal-buy]")};function t(){e.modal.classList.toggle("is-hidden")}e.closeModalBtn.addEventListener("click",t),document.addEventListener("click",(e=>{e.target.matches("[data-modal-buy-open]")&&t()}))})();
+//# sourceMappingURL=index.aee99c79.js.map
